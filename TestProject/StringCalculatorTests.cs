07e51366962c3b157f6ec66ecd8619fa03815db9
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2023-02-27 09:18:02 +0100

    Add project files.

---
+++ TestProject/StringCalculatorTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab_io2;
+
+namespace TestProject
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int result = StringCalculator.SumString("");
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData("12,12", 24)]
+        [InlineData("1,25",26)]
+        [InlineData("225,100",325)]
+        public void SingleNumberReturnsValue(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData("12\n12", 24)]
+        [InlineData("1\n25", 26)]
+        [InlineData("225\n100", 325)]
+        public void StringSumNewLine(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData("12\n12,1", 25)]
+        [InlineData("1,13\n25", 39)]
+        [InlineData("0,1,2", 3)]
+        public void StringSumNewLineOrComma(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData("12\n12,-1", 25)]
+        [InlineData("1,-13\n25", 39)]
+        [InlineData("0,1,-2", 3)]
+        public void NegativeValThrowsArgException(string str, int expectedValue)
+        {
+            Assert.Throws<ArgumentException>(() => StringCalculator.SumString(str));
+        }
+
+        [Theory]
+        [InlineData("12\n12,1000", 1024)]
+        [InlineData("1,1300\n25", 26)]
+        [InlineData("0,1,2000", 1)]
+        public void ShouldIgnoreGreaterThan1000(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData("12\n12,1000", 1024)]
+        [InlineData("1,1300\n25", 26)]
+        [InlineData("0,1,2000", 1)]
+        public void ShouldIgnoreGreaterThan1000(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue, result);
+        }
+    }
+}
---
+++ lab_io2/StringCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_io2
+{
+    public class StringCalculator
+    {
+        public static int SumString(string str)
+        {
+            if(str == string.Empty)
+                return 0;
+
+            int sum = 0;
+
+            if (int.TryParse(str, out sum))
+            {
+                if (sum < 0)
+                    throw new ArgumentException();
+
+                if (sum > 1000)
+                    return 0;
+
+                return sum;
+            }
+
+            char separator = ',';
+            if(str.Substring(0, 2) == "//")
+            {
+                if (str[3] == '\n')
+                    separator = str[2];
+            }
+            var numbers = str.Split(',', '\n', separator);
+            foreach(var number in numbers)
+            {
+                int parsedNum = int.Parse(number);
+
+                if(parsedNum < 0)
+                    throw new ArgumentException();
+
+                if (parsedNum > 1000)
+                    parsedNum = 0;
+
+                sum += parsedNum;
+            }
+
+
+
+            return sum;
+        }
+    }
+}
